16e20ad92b43e2895aa9f9a858e314574a57cba6
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-04-17 11:28:51 +0200

    feat: add transpose selector component

--- src/components/Pattern.svelte
+++ src/components/Pattern.svelte
@@ -1,26 +1,40 @@
 <script lang="ts">
 	import PhraseSelector from "./PhraseSelector.svelte";
+	import TransposeSelector from "./TransposeSelector.svelte";
 	import VerticalNumbers from "./VerticalNumbers.svelte";
-	import { createActivePatternState, createPatternsState } from "./globalState.svelte";
+	import {
+		createActivePatternState,
+		createPatternsState,
+		createTransposePatternsState
+	} from "./globalState.svelte";
 	import { toHex } from "./utils";
 
 	// let channels = [1];
 	let rows = Array.from({ length: 16 });
 
-  let patternState = createPatternsState();
-  let activePattern = createActivePatternState();
-  // $inspect(activePatternElementId.value)
+	let patternState = createPatternsState();
+	let transposePatternsState = createTransposePatternsState();
+	let activePattern = createActivePatternState();
+	// $inspect(patternState.value[activePattern.value])
 </script>
 
 <main class="flex overflow-hidden">
 	<VerticalNumbers />
 	<div class="flex flex-col">
 		{#each rows as _, i}
-			<div id={`row-${i}`} class="flex gap-2">
-				<PhraseSelector 
-        selectedPhrase={patternState.value[toHex(i) as keyof typeof patternState.value]?.[activePattern.value] ?? "--"}
+			<div class="flex flex-row gap-12">
+				<div id={`row${i}`} class="flex gap-2">
+					<PhraseSelector
+						selectedPhrase={patternState.value[activePattern.value as keyof typeof patternState.value]?.[toHex(i) as keyof typeof patternState.value] ?? "--"}
 						hex={toHex(i)}
-        id={`row${i}-pattern`} />
+						id={`row${i}-pattern`} />
+				</div>
+				<div id={`row${i}-transpose-div`} class="flex gap-2 bg-primary-500">
+					<TransposeSelector
+						transpose={transposePatternsState.value[activePattern.value as keyof typeof patternState.value]?.[toHex(i) as keyof typeof patternState.value] ?? "00"}
+						hex={toHex(i)}
+						id={`row${i}-transpose`} />
+				</div>
 			</div>
 		{/each}
 	</div>
---
+++ src/components/TransposeSelector.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import {
+		createActivePatternElementIdState,
+		createSPressedState,
+		createDPressedState,
+		createFPressedState,
+		createActivePatternState,
+		createActivePhraseState,
+		createActivePhraseElementIdState
+	} from "./globalState.svelte";
+
+	let { id, hex, transpose }: { id: string; hex: string; transpose: string } = $props();
+	// let name = $state("--");
+	let activePhraseElementId = createActivePhraseElementIdState();
+	let activePhrase = createActivePhraseState();
+	let sPressed = createSPressedState();
+	let dPressed = createDPressedState();
+	let fPressed = createFPressedState();
+
+	// let el = document.getElementById("div-1").nextSibling;
+
+	function focusPhraseSelector({ row }: { row: number }) {
+		try {
+			const phraseSelector: HTMLButtonElement = document.querySelector(`#row${row}-pattern`)!;
+			phraseSelector.focus();
+			activePhraseElementId.value = phraseSelector.id;
+			activePhrase.value = phraseSelector.innerText;
+		} catch (error) {
+			console.error(`element ${`#row${row}-pattern`} not found, can't focus`);
+		}
+	}
+
+	function focusTransposeSelector({ row }: { row: number }) {
+		try {
+			const transposeSelector: HTMLButtonElement = document.querySelector(`#row${row}-transpose`)!;
+			transposeSelector.focus();
+			activePhraseElementId.value = transposeSelector.id;
+			activePhrase.value = transposeSelector.innerText;
+		} catch (error) {
+			console.error(`element ${`#row${row}`} not found, can't focus`);
+		}
+	}
+
+	function handleKeyPress(e: KeyboardEvent) {
+		const row = id.split("row")[1].split("-channel")[0];
+		const channel = id.split("row")[1].split("-channel")[1];
+		e.preventDefault();
+		// if s, d or f are pressed, don't use below logic that switches cursor location
+		if (sPressed.value || dPressed.value || fPressed.value) return;
+
+		if (e.code === "ArrowLeft") {
+			focusPhraseSelector({ row: parseInt(row) });
+		} else if (e.code === "ArrowRight") {
+			// focusPatternSelector({ row: parseInt(row), channel: parseInt(channel) + 1 });
+		} else if (e.code === "ArrowUp") {
+			focusTransposeSelector({ row: parseInt(row) - 1 });
+		} else if (e.code === "ArrowDown") {
+			focusTransposeSelector({ row: parseInt(row) + 1 });
+		}
+	}
+
+	function handleClick(e: MouseEvent) {
+		activePhraseElementId.value = (<HTMLButtonElement>e.target).id;
+	}
+</script>
+
+<button
+	{id}
+	data-hex={hex}
+	onkeydown={handleKeyPress}
+	onclick={handleClick}
+	class:text-lg={transpose !== "--"}
+	class:items-center={transpose !== "--"}
+	class="focus:ring-none focus-visible:ring-none focus:bg-selection-500 relative flex h-5 w-6 justify-center border-none p-0 text-3xl text-white focus:text-black focus:outline-transparent focus-visible:border-none focus-visible:outline-none">
+	<span class:leading-={transpose !== "--"} class="pointer-events-none leading-3">
+		{transpose}
+	</span>
+</button>
+
+<style>
+</style>
