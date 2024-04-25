7b18a570a539578f1ce20b9d8570439e5af4b7c8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-04-25 15:54:55 +0200

    feat: add support for adding patterns, removing patterns/phrases, adding last touched phrase/note

--- src/components/editing.svelte.ts
+++ src/components/editing.svelte.ts
@@ -3,15 +3,19 @@ import {
 	createLastPatternHexState,
 	createLastPhraseHexState,
 	createLastTouchedNoteState,
+	createLastTouchedPhraseState,
+	createPatternsState,
 	createPhrasesState,
 	marimba
 } from "./globalState.svelte";
-import { toHex } from "./utils";
+import { toHex, toInt } from "./utils";
 
 let activeScreen = createActiveScreenState();
-let lastPattern = createLastPatternHexState();
-let lastPhrase = createLastPhraseHexState();
+let lastPatternHex = createLastPatternHexState();
+let lastPhraseHex = createLastPhraseHexState();
+let lastTouchedPhrase = createLastTouchedPhraseState();
 let lastTouchedNote = createLastTouchedNoteState();
+let patterns = createPatternsState();
 let phrases = createPhrasesState();
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
@@ -20,26 +24,52 @@ type addProps = {
 };
 
 export function add({ element }: addProps) {
+	if (activeScreen.value === "pattern") {
+		const row = element.id.split("row")[1].split("-pattern")[0];
+		const pattern = element.id.split("row")[1].split("-pattern")[1];
+
+		if (element.innerText === "--") {
+			console.log("new pattern here");
+
+			// initialize empty object if they do not exist
+			if (!patterns.value) {
+				patterns.value = {};
+			}
+			if (!patterns.value[lastPatternHex.value]) {
+				patterns.value[lastPatternHex.value] = {};
+			}
+			if (!patterns.value[lastPatternHex.value][toHex(+row)]) {
+				patterns.value[lastPatternHex.value][toHex(+row)] = "";
+			}
+
+			// lastPhraseHex
+
+			// console.log(phrases.value[lastPatternHex.value]);
+
+			patterns.value[lastPatternHex.value][toHex(+row)] = lastTouchedPhrase.value;
+		} else {
+		}
+	}
+
 	if (activeScreen.value === "phrase") {
 		const row = element.id.split("note")[1].split("-channel")[0];
 		const channel = element.id.split("note")[1].split("-channel")[1];
 
 		if (element.innerText === "---") {
 			console.log("new note here");
-			console.log(phrases.value[lastPhrase.value][toHex(+channel)]);
 
 			// initialize empty object if they do not exist
 			if (!phrases.value) {
 				phrases.value = {};
 			}
-			if (!phrases.value[lastPhrase.value]) {
-				phrases.value[lastPhrase.value] = {};
+			if (!phrases.value[lastPhraseHex.value]) {
+				phrases.value[lastPhraseHex.value] = {};
 			}
-			if (!phrases.value[lastPhrase.value][toHex(+channel)]) {
-				phrases.value[lastPhrase.value][toHex(+channel)] = {};
+			if (!phrases.value[lastPhraseHex.value][toHex(+channel)]) {
+				phrases.value[lastPhraseHex.value][toHex(+channel)] = {};
 			}
 
-			phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] = "C3";
+			phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] = lastTouchedNote.value;
 		} else {
 		}
 	}
@@ -53,7 +83,7 @@ type previewProps = {
 export function preview({ element, instrument }: previewProps) {
 	const row = element.id.split("note")[1].split("-channel")[0];
 	const channel = element.id.split("note")[1].split("-channel")[1];
-	let selectedNote = phrases.value[lastPhrase.value]?.[toHex(+channel)]?.[toHex(+row)];
+	let selectedNote = phrases.value[lastPhraseHex.value]?.[toHex(+channel)]?.[toHex(+row)];
 	if (selectedNote === undefined) return;
 	marimba.start({ note: selectedNote });
 }
@@ -64,28 +94,71 @@ type editProps = {
 };
 
 export function edit({ direction, element }: editProps) {
-	const row = element.id.split("note")[1].split("-channel")[0];
-	const channel = element.id.split("note")[1].split("-channel")[1];
-	let selectedNote = phrases.value[lastPhrase.value]?.[toHex(+channel)]?.[toHex(+row)];
-	if (selectedNote === undefined) return;
-	const selectedNoteTone = selectedNote.split(selectedNote[selectedNote.length - 1])[0];
-	const selectedNoteOctave = selectedNote[selectedNote.length - 1];
-	let newNoteTone;
-	let newNoteOctave;
+	if (activeScreen.value === "pattern") {
+		const row = element.id.split("row")[1].split("-pattern")[0];
+		const pattern = lastPatternHex.value;
+		let selectedPhrase = element.innerText;
+		console.table({ row, pattern, selectedPhrase });
+		if (selectedPhrase === undefined) return;
+		// const selectedPhraseTone = selectedPhrase.split(selectedPhrase[selectedPhrase.length - 1])[0];
+		let newPhrase;
+		if (direction === "right") {
+			if (selectedPhrase === "FF") {
+				selectedPhrase = "00";
+				patterns.value[lastPatternHex.value][toHex(+row)] = `${toHex(toInt(selectedPhrase))}`;
+			} else {
+				patterns.value[lastPatternHex.value][toHex(+row)] = `${toHex(toInt(selectedPhrase) + 1)}`;
+			}
+		} else if (direction === "left") {
+			if (selectedPhrase === "00") {
+				selectedPhrase = "FF";
+				patterns.value[lastPatternHex.value][toHex(+row)] = `${toHex(toInt(selectedPhrase))}`;
+			} else {
+				patterns.value[lastPatternHex.value][toHex(+row)] = `${toHex(toInt(selectedPhrase) - 1)}`;
+			}
+		} else if (direction === "up") {
+			if (toInt(selectedPhrase) > 243) {
+				patterns.value[lastPatternHex.value][toHex(+row)] =
+					`${toHex(toInt(selectedPhrase) - 256 + 12)}`;
+			} else {
+				patterns.value[lastPatternHex.value][toHex(+row)] = `${toHex(toInt(selectedPhrase) + 12)}`;
+			}
+		} else if (direction === "down") {
+			if (toInt(selectedPhrase) < 12) {
+				patterns.value[lastPatternHex.value][toHex(+row)] =
+					`${toHex(toInt(selectedPhrase) + 256 - 12)}`;
+			} else {
+				patterns.value[lastPatternHex.value][toHex(+row)] = `${toHex(toInt(selectedPhrase) - 12)}`;
+			}
+		}
+		// ensure we get the updated value
+		setTimeout(() => {
+			lastTouchedPhrase.value = (<HTMLButtonElement>document.activeElement).innerText;
+		}, 0);
+	}
 
-	if (activeScreen.value === "phrase")
+	if (activeScreen.value === "phrase") {
+		const row = element.id.split("note")[1].split("-channel")[0];
+		const channel = element.id.split("note")[1].split("-channel")[1];
+		let selectedNote = phrases.value[lastPhraseHex.value]?.[toHex(+channel)]?.[toHex(+row)];
+		if (selectedNote === undefined) return;
+		const selectedNoteTone = selectedNote.split(selectedNote[selectedNote.length - 1])[0];
+		const selectedNoteOctave = selectedNote[selectedNote.length - 1];
+		let newNoteTone;
+		let newNoteOctave;
+
 		if (direction === "right") {
 			// console.log(row, channel);
 			if (notes.indexOf(selectedNoteTone) === notes.length - 1) {
 				if (+selectedNoteOctave > 6) return;
 				newNoteTone = "C";
 				newNoteOctave = +selectedNoteOctave + 1;
-				phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] =
+				phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] =
 					`${newNoteTone}${newNoteOctave}`;
 			} else {
 				newNoteTone = notes[notes.indexOf(selectedNoteTone) + 1];
 				newNoteOctave = +selectedNoteOctave;
-				phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] =
+				phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] =
 					`${newNoteTone}${newNoteOctave}`;
 			}
 		} else if (direction === "left") {
@@ -93,25 +166,53 @@ export function edit({ direction, element }: editProps) {
 				if (+selectedNoteOctave <= 0) return;
 				newNoteTone = "B";
 				newNoteOctave = +selectedNoteOctave - 1;
-				phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] =
+				phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] =
 					`${newNoteTone}${newNoteOctave}`;
 			} else {
 				newNoteTone = notes[notes.indexOf(selectedNoteTone) - 1];
 				newNoteOctave = +selectedNoteOctave;
-				phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] =
+				phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] =
 					`${newNoteTone}${newNoteOctave}`;
 			}
 		} else if (direction === "up") {
 			if (+selectedNoteOctave > 6) return;
 			newNoteTone = selectedNoteTone;
 			newNoteOctave = +selectedNoteOctave + 1;
-			phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] =
+			phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] =
 				`${newNoteTone}${newNoteOctave}`;
 		} else if (direction === "down") {
 			if (+selectedNoteOctave <= 0) return;
 			newNoteTone = selectedNoteTone;
 			newNoteOctave = +selectedNoteOctave - 1;
-			phrases.value[lastPhrase.value][toHex(+channel)][toHex(+row)] =
+			phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] =
 				`${newNoteTone}${newNoteOctave}`;
 		}
+
+		// ensure we get the updated value
+		setTimeout(() => {
+			lastTouchedNote.value = (<HTMLButtonElement>document.activeElement).innerText.replaceAll(
+				// ! this is probably stupid but it works (C  3 turns into C3 etc)
+				" ",
+				""
+			);
+		}, 0);
+	}
+}
+
+type deleteProps = {
+	element: HTMLButtonElement;
+};
+
+export function remove({ element }: deleteProps) {
+	if (activeScreen.value === "pattern") {
+		const row = element.id.split("row")[1].split("-pattern")[0];
+		const pattern = element.id.split("row")[1].split("-pattern")[1];
+		patterns.value[lastPatternHex.value][toHex(+row)] = `--`;
+	}
+
+	if (activeScreen.value === "phrase") {
+		const row = element.id.split("note")[1].split("-channel")[0];
+		const channel = element.id.split("note")[1].split("-channel")[1];
+		phrases.value[lastPhraseHex.value][toHex(+channel)][toHex(+row)] = `---`;
+	}
 }
