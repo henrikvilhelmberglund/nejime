e83880280ae2d8f3fb92f86a5df1d5bfc6efcc40
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-05-03 17:35:59 +0200

    feat: implement song playback (WIP), add button to save song as base64 string, add link to the demo song

--- src/components/Nejime.svelte
+++ src/components/Nejime.svelte
@@ -20,9 +20,15 @@
 		createPatternsState,
 		createPhrasesState,
 		createPlayPositionPhraseState,
-		createPlayPositionPatternState
+		createPlayPositionPatternState,
+		saveSong,
+		createSongState,
+		createPlayPositionsPhrasesState,
+		createPlayPositionsPatternsState,
+		createPlayPositionsSongState,
+		createIntervalIdsSongState
 	} from "./globalState.svelte";
-	import { playPhrase, stop, toHex } from "./utils";
+	import { playPhrase, playPhraseFromSong, stop, toHex } from "./utils";
 
 	let allStates = ["song", "pattern", "phrase", "instrument", "project-song", "project-pattern"];
 	let activeScreenState = createActiveScreenState();
@@ -55,14 +61,20 @@
 			activeScreenState.value = "pattern";
 		}
 	}
-	const intervalIdState = createIntervalIdState();
-	const phraseLoopIntervalIdState = createPhraseLoopIntervalIdState();
+	const intervalId = createIntervalIdState();
+	const phraseLoopIntervalId = createPhraseLoopIntervalIdState();
 	let lastPhraseHex = createLastPhraseHexState();
 	let lastPatternHex = createLastPatternHexState();
-	let patternsState = createPatternsState();
-	let phrasesState = createPhrasesState();
+	let song = createSongState();
+	let patterns = createPatternsState();
+	let phrases = createPhrasesState();
 	let playPositionPhrase = createPlayPositionPhraseState();
 	const playPositionPattern = createPlayPositionPatternState();
+
+	let playPositionsPhrases = createPlayPositionsPhrasesState();
+	let playPositionsPatterns = createPlayPositionsPatternsState();
+	let playPositionsSong = createPlayPositionsSongState();
+	let intervalIdsSongState = createIntervalIdsSongState();
 
 	function handleKeyDown(e: KeyboardEvent) {
 		// console.log(e);
@@ -89,6 +101,66 @@
 		}
 		if (e.code === "Space") {
 			if (!isPlayingBack.value) {
+				if (activeScreenState.value === "song") {
+					// ? caused issue when editing notes while phrase was playing
+					// play(activeScreenState.value, lastPhraseHex.value);
+					// after first play, start looping
+					// phraseLoopIntervalIdState.value = setInterval(
+					// 	() => {
+					// 		play(activeScreenState.value, lastPhraseHex.value);
+					// 	},
+					// 	1000 * 16 * (60 / (bpmState.value * 4))
+					// );
+
+					if (e.ctrlKey) {
+						const position = document.activeElement!.id.split("row")[1].split("-pattern")[0];
+						console.log("position", position);
+						playPositionPattern.value = parseInt(position);
+					} else {
+						playPositionPattern.value = 0;
+					}
+
+					["channel0", "channel1", "channel2", "channel3", "channel4"].forEach((channel, i) => {
+						// console.table(phrasesToPlay);
+						console.log("i", i);
+						const patternsToPlay = song.value["00"];
+						const phrasesToPlay = patterns.value?.[patternsToPlay[channel]];
+						console.log("phrasesToPlay", phrasesToPlay);
+						if (phrasesToPlay) {
+							playPhraseFromSong(
+								activeScreenState.value,
+								phrasesToPlay[toHex(playPositionsPatterns.value[i])],
+								i
+							);
+
+							intervalIdsSongState.value[i] = setInterval(
+								() => {
+									if (playPositionsPhrases.value[i] < 15) {
+										playPositionsPhrases.value[i] += 1;
+										playPhraseFromSong(
+											activeScreenState.value,
+											phrasesToPlay[toHex(playPositionsPatterns.value[i])],
+											i
+										);
+									} else {
+										playPositionsPatterns.value[i] += 1;
+										if (!phrasesToPlay[toHex(playPositionsPatterns.value[i])]) {
+											playPositionsPatterns.value[i] = 0;
+										}
+
+										playPositionsPhrases.value[i] = 0;
+										playPhraseFromSong(
+											activeScreenState.value,
+											phrasesToPlay[toHex(playPositionsPatterns.value[i])],
+											i
+										);
+									}
+								},
+								1000 * (60 / (bpmState.value * 4))
+							);
+						}
+					});
+				}
 				if (activeScreenState.value === "pattern") {
 					// ? caused issue when editing notes while phrase was playing
 					// play(activeScreenState.value, lastPhraseHex.value);
@@ -100,7 +172,7 @@
 					// 	1000 * 16 * (60 / (bpmState.value * 4))
 					// );
 
-					const phrasesToPlay = patternsState.value[lastPatternHex.value];
+					const phrasesToPlay = patterns.value[lastPatternHex.value];
 
 					if (e.ctrlKey) {
 						const position = document.activeElement!.id.split("row")[1].split("-pattern")[0];
@@ -113,7 +185,7 @@
 					// console.table(phrasesToPlay);
 					playPhrase(activeScreenState.value, phrasesToPlay[toHex(playPositionPattern.value)]);
 
-					intervalIdState.value = setInterval(
+					intervalId.value = setInterval(
 						() => {
 							if (playPositionPhrase.value < 15) {
 								playPositionPhrase.value += 1;
@@ -157,7 +229,7 @@
 					}
 					playPhrase(activeScreenState.value, lastPhraseHex.value);
 
-					intervalIdState.value = setInterval(
+					intervalId.value = setInterval(
 						() => {
 							if (playPositionPhrase.value < 15) {
 								playPositionPhrase.value += 1;
@@ -174,13 +246,25 @@
 			} else {
 				stop(activeScreenState.value);
 				isPlayingBack.value = false;
-				intervalIdState.stop();
-				phraseLoopIntervalIdState.stop();
+				intervalIdsSongState.stopAllIntervals();
+				intervalId.stop();
+				phraseLoopIntervalId.stop();
 			}
 		}
 	}
 </script>
 
+<button
+	onclick={() => {
+		let start = performance.now();
+		const savedSong = saveSong();
+		let timeTaken = performance.now() - start;
+		console.log("Total time taken : " + timeTaken + " milliseconds");
+		console.log(savedSong);
+	}}>Save</button>
+<a
+	href="/N4IgRgDgtiBcCMAmADAGhASwHYGcAuATgK5QCmWeAIkQQIZ4YD2ucoyycyAdAMzrLxOXeAF90OZgHNWIdjIDGAC1pYspADYdYsjuiUq16wdoEg9y1RsRxZ18wY08byJ-cvqALM69jZx0PruWiAoZiCBhsYhgm6G1tqIduEWhk4JrskOnjaIPr4Q9HikBCywbFrlNvC6flUxtlVJLlUZyF7a8F78AKxVvfwAbFVdsgDsfWHIABxVNcgAnHWTAIKNkwBCLSC+pmU6Nh71uyAeTfEnrWknI20H-bK92h5Dg3eT408vsjNPR4tPTVWT1amyeeXQhBUOAgjBwpAACoViqVKns5CY5uc5Px2vsdv59hjJlFsQ0iSJ8oo6HCcDJqjJ0aiQABhG641mTH4gADiN2ZNl5234-gp6HgBMZhJAyzZNhlnIFPmFrB28WAopASAZFSlADFZdp9QrtABRPk2M1C2plVUq3zwNKopmUA0gF3GlngzXtJ1SwU4xUeo38fmGr3wR6+9EgS0B003LnujWHbWp7TM+7TC29ZNqwlM3WZrkmnO+DyO-NSyhFmwZ7Zln2V6PyuMgdYJ2ul9AeSNN4Ilya4-3fGzVyahnle55ppl11vDrPadvjwMUynU0gASVwhBI5DwtL29LRwVJxwAtOfJudL5MrreejYH7JcWeuWeJ3J7RLT3NX3N3w4e082jM9-w9D9nCAsUK1Av8oIg6DvTTfZWzfBCQwQ+1ezgwcMJHclux-ZwjlxZ9F1qTDtFvMsQNPUinyvfh3yOIEQHIicaO7WD6Lw6imIIyjZEoRiV34q0PEbUCGJMI4WLE9irzLHDeLQuSSIU0wyyGE8NLUiCjk-QQ1yAA"
+	>Twinkle Twinkle Little Star</a>
 <div
 	id="nejime"
 	role="presentation"
