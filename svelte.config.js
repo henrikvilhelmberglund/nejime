11db82e2b4a694a9ffdce6e0ae2814fb6929b9fb
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-05-22 18:21:59 +0200

    fix: fix adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -11,11 +11,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-  },
-  vitePlugin: {
-    inspector: true
-  }
+		adapter: adapter({
+			fallback: "200.html" // may differ from host to host
+		})
+	},
+	vitePlugin: {
+		inspector: true
+	}
 };
 
 export default config;
