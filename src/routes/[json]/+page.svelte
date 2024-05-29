393f2dab27b61ff8ea0483756f987804d996d3e1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-05-29 23:42:14 +0200

    fix: styling, handle oncontextmenu (maybe)

--- src/routes/[json]/+page.svelte
+++ src/routes/[json]/+page.svelte
@@ -4,10 +4,27 @@
 	import Nejime from "../../components/Nejime.svelte";
 </script>
 
-<main class="flex flex-col h-screen justify-center items-center dark:bg-black dark:text-white">
-<header>
-	<ThemeInit />
-	<DarkModeInit />
-</header>
+<main
+	oncontextmenu={(e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		e.stopImmediatePropagation();
+    return false;
+	}}
+	class="flex h-[86vh] flex-col items-center bg-hero-bathroom-floor-blue-500/5 lg:justify-center dark:bg-black dark:text-white">
+	<header>
+		<ThemeInit />
+		<DarkModeInit />
+	</header>
 	<Nejime />
 </main>
+
+<style>
+	:root {
+		touch-action: pan-x pan-y;
+		height: 100%;
+    user-select: none;
+    -webkit-user-select: none;
+    -webkit-user-drag: none;
+	}
+</style>
